7e1f9a6560464854a14fa7669a94ed0d3a097d1e
Sangam Pandey <sangampandey.wh@gmail.com>
2026-04-04 20:35:46 +0545

    python

---
+++ test.py
@@ -0,0 +1,6 @@
+# wap to pritn the sum of two numbers in python
+# to run this program in vscode terminal; uv run test.py
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+sum = num1 + num2
+print("The sum is:", sum)
