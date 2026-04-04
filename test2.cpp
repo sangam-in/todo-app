42b7b8293c064fb90ddee845fa62f6b8e096df71
Sangam Pandey <sangampandey.wh@gmail.com>
2026-04-04 19:55:46 +0545

    clear

--- test2.cpp
+++
@@ -1,7 +0,0 @@
-// simple c++ program to print hello world
-#include <iostream>
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
--- test3.cpp
+++
@@ -1,7 +0,0 @@
-// wap simple print c progam
-#include <iostream>
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
