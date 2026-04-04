329f1467f87c13feec28ced4afafacf0c9d29697
unknown <sangampandey.in@gmail.com>
2026-04-04 19:31:25 +0545

    js

---
+++ script.js
@@ -0,0 +1,6 @@
+// write simple todo app with html, css and js
+
+// Get references to DOM elements
+const todoInput = document.getElementById('todo-input');
+const addButton = document.getElementById('add-button');
+const todoList = document.getElementById('todo-list');
